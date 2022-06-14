c9530c49a401a99e21c7a81790efd2ed5e799e92
davidriver <dav-rc@hotmail.com>
2022-06-13 23:42:14 -0500

    Implement functionality
    -Implemented functional display
    -Implemented functional mini-display
    -Implemented functional operations with input
    -Implemented calculations with multiple operations
    -Calculator evaluates a single operation at a time
    -Rounds big decimal results to 2 places
    -No erros pressing '=' early
    -Implemented clear functionality
    -No crashes when dividing by zero
    -Implemented a dot button for floats and implemented disable to not
        allow multiple dots in one number
    -Implemented nice visual appearance
    -Implemented backspace button

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,27 @@
+const decimalCount = num =>  {
+    const numStr = String(num);
+    if (numStr.includes('.')){
+        return numStr.split('.')[1].length;
+    } else {
+        return 0;
+    }
+}
+
+function toggleDot (){
+    let dotBtn = document.querySelector('#btn14');
+    if (dotOn){
+        dotBtn.replaceWith(dotBtn.cloneNode(true));
+        dotOn = false;
+    } else {
+        dotBtn.addEventListener('click', function(){
+            displayValue = addToDisplay(dotBtn.textContent, displayValue);
+            display.textContent = displayValue;
+            toggleDot();
+        })
+        dotOn = true;
+    }
+}
+    
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -7,22 +31,109 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 function operate(operator, a, b){
+    let result;
     if (operator == '+'){
-        return add(a, b);
+        result = add(a, b);
     } else if (operator == '-'){
-        return subtract(a, b);
-    } else if (operator == '*'){
-        return multiply(a, b);
+        result = subtract(a, b);
+    } else if (operator == 'x'){
+        result = multiply(a, b);
     } else if (operator == '/'){
-        return divide(a, b);
+        result = divide(a, b);
     } else {
         return 'Error, operator not valid.'
+    } 
+    if (decimalCount(result) <= 2){
+        return result;
+    } else {
+        return parseFloat(result.toFixed(2));
+    }
+
+}
+
+function addToDisplay(value, display){
+    display += value;
+    return display;
+}
+
+function clearDisplay(){
+    displayValue = '';
+    display.textContent = displayValue;
+    miniDisplayValue = '';
+    miniDisplay.textContent = miniDisplayValue;
+    tempNum1 = undefined;
+    tempNum2 = undefined;
+    tempOperator = undefined;
+    tempResult = undefined;
+}
+
+function deleteLast(){
+    displayValue = displayValue.slice(0, -1);
+    display.textContent = displayValue;
+}
+
+function operatorClicked(operator){
+    if (!tempNum1){
+        tempNum1 = parseFloat(displayValue);
+        tempOperator = operator.textContent;
+        miniDisplayValue = displayValue + ' ' + operator.textContent;
+        miniDisplay.textContent = miniDisplayValue;
+        displayValue = '';
+        display.textContent = displayValue;
+    } else {
+        tempNum2 = parseFloat(displayValue);
+        tempResult = operate(tempOperator, tempNum1, tempNum2);
+        tempOperator = operator.textContent;
+        tempNum1 = tempResult;
+        miniDisplayValue = tempResult + ' ' + operator.textContent;
+        miniDisplay.textContent = miniDisplayValue;
+        displayValue = '';
+        display.textContent = displayValue;
     }
 }
+
+function equalClicked(){
+    tempNum2 = parseFloat(displayValue);
+    tempResult = operate(tempOperator, tempNum1, tempNum2);
+    displayValue = tempResult;
+    display.textContent = displayValue;
+    miniDisplayValue = '';
+    miniDisplay.textContent = miniDisplayValue;
+    tempNum1 = undefined;
+    tempNum2 = undefined;
+    tempOperator = undefined;
+}
+
+let dotOn = true;
+const display = document.querySelector('#mainDisplay');
+const miniDisplay = document.querySelector('#miniDisplay');
+let tempNum1, tempNum2, tempOperator, tempResult;
 
 //Create calculator
 let z = 1;
 const cont = document.querySelector('#btnContainer');
+let firstLine = document.createElement('div')
+firstLine.className = 'line';
+firstLine.setAttribute('id', 'firstLine');
+btnContainer.appendChild(firstLine);
+clearBtn = document.createElement('button');
+clearBtn.className = 'key';
+clearBtn.setAttribute('id', 'clearBtn');
+clearBtn.textContent = 'clear';
+clearBtn.addEventListener('click', function(){
+    clearDisplay();
+    toggleDot();
+});
+firstLine.appendChild(clearBtn);
+delBtn = document.createElement('button');
+delBtn.className = 'key';
+delBtn.setAttribute('id', 'delBtn');
+delBtn.textContent = 'delete';
+delBtn.addEventListener('click', function(){
+    deleteLast();
+})
+firstLine.appendChild(delBtn);
+
 for (let x=0; x<4; x++){
     let line = document.createElement('div');
     line.className = 'line';
@@ -33,6 +144,29 @@ for (let x=0; x<4; x++){
         button.className = 'key';
         button.setAttribute('id', `btn${z}`);
         z++;
+        button.addEventListener('click', function(){
+            if (isNaN(button.textContent) && button.textContent != '.'){
+                if (button.textContent == '='){
+                    if (miniDisplay.textContent){
+                        equalClicked();
+                        if (!dotOn){
+                            toggleDot();
+                        }
+                    }
+                } else {
+                    operatorClicked(button);
+                    if (!dotOn){
+                        toggleDot();
+                    }
+                }
+            } else {
+                displayValue = addToDisplay(button.textContent, displayValue);
+                display.textContent = displayValue;
+                if (button.textContent == '.'){
+                    toggleDot();
+                }
+            }
+        })
         line.appendChild(button);
     }
 }
@@ -52,3 +186,6 @@ btn13.textContent = '0';
 btn14.textContent = '.';
 btn15.textContent = '+';
 btn16.textContent = '=';
+
+displayValue = '';
+
